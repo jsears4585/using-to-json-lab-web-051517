090f5c5461bbc2333edbb6e735bd0fbe99e3bc97
Jason Sears <jsears4585@gmail.com>
2017-07-06 14:03:10 -0400

    Done.

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,13 +1,3 @@
-class ProductSerializer
-  def self.serialize(product)
-    serialized_product = '{'
-
-    serialized_product += '"id": ' + product.id.to_s + ', '
-    serialized_product += '"name": "' + product.name + '", '
-    serialized_product += '"description": "' + product.description + '", '
-    serialized_product += '"inventory": ' + product.inventory.to_s + ', '
-    serialized_product += '"price": "' + product.price.to_s + '"'
-
-    serialized_product += '}'
-  end
+class ProductSerializer < ActiveModel::Serializer
+  attributes :id, :name, :description, :inventory, :price
 end
